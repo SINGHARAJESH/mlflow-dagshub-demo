c3159a3beaeccb8ab707fed39d8c50905e39e034
SINGHARAJESH <rajeshsingha1947@gmail.com>
2025-05-31 09:28:28 +0530

    second initial commit

--- iris_dt.py
+++ iris_dt.py
@@ -7,10 +7,11 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#import dagshub
-#dagshub.init(repo_owner='campusx-official', repo_name='mlflow-dagshub-demo', mlflow=True)
+import dagshub
+dagshub.init(repo_owner='rajeshai2000', repo_name='mlflow-dagshub-demo', mlflow=True)
 
-#mlflow.set_tracking_uri("https://dagshub.com/campusx-official/mlflow-dagshub-demo.mlflow")
+
+mlflow.set_tracking_uri("https://github.com/SINGHARAJESH/mlflow-dagshub-demo.git")
 
 # Load the iris dataset
 iris = load_iris()
